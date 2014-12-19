7cf4274f0293461c0889ce343834b94d9669ffa2
wap51 <bourg.paul@gmail.com>
2014-12-19 16:51:59 +0100

    soundBipbip

---
+++ www/js/index.js
@@ -0,0 +1,32 @@
+document.addEventListener('deviceready', function() {
+
+    var bipbipSound = new Media
+    (
+        cordova.file.applicationDirectory + 'www/audio/bipbip.wav',
+        mediaSuccess,
+        mediaError,
+        mediaStatus
+    );
+
+    function mediaSuccess()
+    {
+
+    }
+
+    function mediaError(error)
+    {
+        alert(error);
+    }
+
+    function mediaStatus()
+    {
+
+    }
+
+    document.querySelector('button').addEventListener('click',function()
+    {
+        bipbipSound.play();
+    });
+
+
+});
